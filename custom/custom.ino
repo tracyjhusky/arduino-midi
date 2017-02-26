b4b24747f1ec6675110807da15e842389c3adb47
James Tracy <tracy.j@husky.neu.edu>
2017-02-26 13:47:55 -0500

    Better lights

--- custom/custom.ino
+++ custom/custom.ino
@@ -6,16 +6,18 @@ const int BAUD = 38400;
 const int PIXELS = 60;
 const int PIN = 6;
 
+int count = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELS, PIN);
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 void consumeNote(byte channel, byte pitch, byte velocity) {
-  for(int i = 0; i < PIXELS; i++) {
-    strip.setPixelColor(i, 0, 255, 255);
+  if(count < PIXELS) {
+    strip.setPixelColor(count, 0, 255, 255);
+    strip.show();
+    count++;
   }
-  strip.show();
 }
 
 void setup() {
