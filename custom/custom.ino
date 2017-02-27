905f305596deaf0f62207dd236f77b84837aef77
James Tracy <tracy.j@husky.neu.edu>
2017-02-26 21:02:03 -0500

    HSV conversion

--- custom/custom.ino
+++ custom/custom.ino
@@ -31,11 +31,56 @@ void loop() {
     command = Serial.read();
     note = Serial.read();
     velocity = Serial.read();
-
-    if(command == 128 && count < PIXELS) {
-      strip.setPixelColor(count, note, 0, 0);
-      strip.show();
-      count++;
+    if(command == 128) {
+      for(int i = 0; i < PIXELS; i++) {
+        setHSV(i, velocity * 2, .5, .5);
+      }
     }
   }
+}
+
+void setHSV(int i, int h, double s, double v) {
+  double c = v * s;
+  double x = c * (1 - Math.fabs((h / 60) % 2 - 1));
+  double m = v - c;
+  double r;
+  double g;
+  double b;
+
+  if(h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  }
+  else if(h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  }
+  else if(h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  }
+  else if(h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  }
+  else if(h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  }
+  else {
+    r = c;
+    g = 0;
+    b = x;
+  }
+
+  int red = (r + m) * 255;
+  int green = (g + m) * 255;
+  int blue = (b + m) * 255;
+
+  strip.setPixelColor(i, red, green, blue);
 }
